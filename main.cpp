8d2c71f9d199ef3a7992b0cd636490f69729f764
Baggins800 <omega@live.co.za>
2015-02-24 13:48:11 -0800

    Main Function

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/** *********************************************************************** **/
+/** *********************************************************************** **/
+/**     Created by:     Ruan (Baggins) Luies 23511354                       **/
+/**     email:          23511354@nwu.ac.za                                  **/
+/**     Project name:   Virtual Concierge Creator And Management System     **/
+/**     File Name:      main.cpp                                            **/
+/**     From Date:      2015/02/24                                          **/
+/**     To Date:        2015/10/01                                          **/
+/** **********************************************************************  **/
+/** *********************************************************************** **/
+
+#include <QApplication>
+#include <QtQuick/QQuickView>
+#include "VCCMUI/BackBone.h"
+
+//This is the main function of the program, everything happens here
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QQuickView view;
+    QSurfaceFormat format;// Antialiasing
+    format.setSamples(4);// Antialiasing
+    view.setFormat(format);// Antialiasing
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    qmlRegisterType<BackBone>("QMLOpenGL", 1, 0, "OpenGL");
+    QObject::connect((QObject*)view.engine(), SIGNAL(quit()), &app, SLOT(quit()));
+    view.setSource(QUrl("qrc:///Menu"));
+    view.show();
+    return app.exec();
+}
