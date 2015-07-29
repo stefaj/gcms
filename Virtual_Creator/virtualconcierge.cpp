b18bc6cb1882ef72fdbd3cbab8b316d11e7d0a9e
Baggins800 <omega@live.co.za>
2015-07-29 12:31:21 -0700

    directories can now direct to each other

--- Virtual_Creator/virtualconcierge.cpp
+++ Virtual_Creator/virtualconcierge.cpp
@@ -20,11 +20,66 @@ void VirtualConcierge::get_button_value(int value,bool findvalue){
     // 0 is the starting position
     if(!findvalue)
     emit find_path(0,value);
+    else show_new_interface(value);
+
+}
+
+void VirtualConcierge::show_new_interface(int value){
+
+      // resizes temp array, but does not release memory!!
+      m_temp.resize(0);
+
+      // hide the buttons from the first page
+      foreach(NodeButton *button, m_catagory)
+          button->hide();
+      foreach(NodeButton *button, m_buttons)
+          button->hide();
+      foreach (NodeButton *button, m_directories)
+          button->hide();
+
+      // add child directories
+      if(value < m_directory_list.count()){
+          QStringList dir_ls =  m_directory_list.value(value).split(";");
+          const int count = dir_ls.count();
+          for(int x= 0;x<count;x++){
+             int dir_index = dir_ls[x]!=""?dir_ls[x].toInt():(-1);
+              if(dir_index > -1) {
+                  NodeButton *button = m_directories.value(dir_index);
+                  button->show();
+                  m_temp.push_back(button);
+              }
+          }
+      }
+
+      // add child paths
+      if(value < m_node_list.count()){
+          QStringList node_ls =  m_node_list.value(value).split(";");
+          const int count = node_ls.count();
+          for(int y= 0;y<count;y++){
+             int node_index = (node_ls[y]!="")?node_ls[y].toInt():(-1);
+              if(node_index > -1) {
+                  int index_from_list = get_index_from_index(m_buttons,node_index);
+                  qDebug()<<index_from_list;
+                  if(index_from_list > -1){
+                      NodeButton *button = m_buttons.value(index_from_list);
+                      button->show();
+                      m_temp.push_back(button);
+                    }
+              }
+          }
+      }
+}
+
+int VirtualConcierge::get_index_from_index(QVector<NodeButton* > list,int index){
+    int temp = -1;
+    for(int l = 0;l < list.count();l++)
+        list.value(l)->getIndex()==index?temp = l: temp=temp;
+
+    return temp;
 }
 
 void VirtualConcierge::create_interface(){
 //    NodeButton *button = new NodeButton(ui->widget_directory);
-//    m_buttons.push_back(button);
     const int width = 150, height = 32;
     int magic_number = floor( ui->widget_directory->height()/height);
     int column = 0, row = -1;
@@ -85,7 +140,11 @@ void VirtualConcierge::load_interface(QString filename, QString filename_directo
         }
 
         if(PremisesExporter::fileExists(filename_directories)){
-
+            // clear directory list
+            m_directory_list.clear();
+            m_node_list.clear();
+            m_catagory.clear();
+            m_directories.clear();
             // load the text file
             QFile textfile_directories(filename_directories);
 
@@ -105,7 +164,7 @@ void VirtualConcierge::load_interface(QString filename, QString filename_directo
                         QStringList dir = list[1].split(":");
                         QString name = dir.count() > 1 ? dir[1] : list[1];
                         int index = list[2].toInt();
-                        count_top++;
+
                         NodeButton *button = new NodeButton(ui->widget_directory);
                         button->setText(name);
                         button->setIndex(index);
@@ -114,6 +173,23 @@ void VirtualConcierge::load_interface(QString filename, QString filename_directo
                         connect(button,SIGNAL(clicked_index(int,bool)), this ,SLOT(get_button_value(int,bool)));
                         m_catagory.push_back(button);
                       }
+                    else if(list[0]=="dl"){
+                        m_directory_list.append(list[3]);
+                        m_node_list.append(list[2]);
+                    } else if(list[0] == "d") {
+
+                        QStringList dir = list[1].split(":");
+                        QString name = dir.count() > 1 ? dir[1] : list[1];
+                        NodeButton *button = new NodeButton(ui->widget_directory);
+                        button->setText(name);
+                        button->setIndex(count_top);
+                        button->setDirectory(dir.count() > 1);
+                        button->setGeometry(1,m_directories.count()*(button->height()+1),button->width(),button->height());
+                        connect(button,SIGNAL(clicked_index(int,bool)), this ,SLOT(get_button_value(int,bool)));
+                        button->hide();
+                        m_directories.push_back(button);
+                        count_top++;
+                    }
 
                     // read next line
                    line = ascread.readLine();
@@ -125,12 +201,7 @@ void VirtualConcierge::load_interface(QString filename, QString filename_directo
         }
 }
 
-VirtualConcierge::~VirtualConcierge(){
-    delete ui;
-}
-
-void VirtualConcierge::on_pushButton_send_mail_clicked()
-{
+void VirtualConcierge::on_pushButton_send_mail_clicked(){
 
     //QString file = QFileDialog::getSaveFileName(this, "Save as...", "name", "PNG (*.png);; BMP (*.bmp);;TIFF (*.tiff *.tif);; JPEG (*.jpg *.jpeg)");
     QImage img =ui->openGLWidget->grabFramebuffer();
@@ -143,3 +214,10 @@ void VirtualConcierge::on_pushButton_send_mail_clicked()
     smtp->sendMail("virtualconcierge@yahoo.com",ui->lineEdit_email->text() , "This is a subject", "This is a body",ls);
 
 }
+
+VirtualConcierge::~VirtualConcierge(){
+    delete ui;
+}
+
+
+
--- Virtual_Creator/virtualconcierge.h
+++ Virtual_Creator/virtualconcierge.h
@@ -23,11 +23,14 @@ private:
     Ui::VirtualConcierge *ui;
     void create_interface();
     void load_interface(QString,QString);
-    QVector<NodeButton *> m_buttons;
-    QVector<NodeButton *> m_catagory;
+    int get_index_from_index(QVector<NodeButton* >,int);
+    QVector<NodeButton *> m_buttons, m_directories;
+    QVector<NodeButton *> m_catagory, m_temp;
+    QVector<QString> m_directory_list,m_node_list;
 private slots:
     void get_button_value(int, bool);
     void on_pushButton_send_mail_clicked();
+    void show_new_interface(int);
 
 signals:
     void find_path(int,int);
