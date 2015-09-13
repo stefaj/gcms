20bae3dc1483f27f6a2d6994c6ab632335c79d23
Ruan <omega@live.co.za>
2015-09-14 01:58:05 +0200

    Visuals improved (second step)

--- Virtual_Creator/main.cpp
+++ Virtual_Creator/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     login login_;
     login_.setWindowFlags(Qt::WindowStaysOnTopHint);
+    //login_.setWindowFlags(Qt::FramelessWindowHint);
     login_.show();
     //MainWindow w;
     //w.showMaximized();
